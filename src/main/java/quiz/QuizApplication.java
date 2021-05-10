24bffde909a57c403808da9a29ad7671253111ac
Nagy Lajos <nagylali001@gmail.com>
2021-05-10 16:53:11 +0200

    menu.fxml modified into mainMenu.fxml

--- src/main/java/quiz/QuizApplication.java
+++ src/main/java/quiz/QuizApplication.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 public class QuizApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
-        stage.setTitle("Menu");
-        stage.setScene(new Scene(root));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainMenu.fxml"));
+        stage.setTitle("Quiz Game");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.setResizable(false);
         stage.show();
 
     }
