e9944fc241e6693e441b10f234be6b038a632d66
Nagy Lajos <nagylali001@gmail.com>
2021-05-10 17:43:55 +0200

    some duplicated code removed from MenuController.java

--- src/main/java/quiz/MenuController.java
+++ src/main/java/quiz/MenuController.java
@@ -13,14 +13,13 @@ import java.io.IOException;
 
 public class MenuController{
 
-    
+
 
     @FXML
     private void switchToMainMenu(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
-        stage.setScene(new Scene(root, 800, 600));
-        stage.setResizable(false);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
@@ -28,8 +27,7 @@ public class MenuController{
     private void switchToQuizForm(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/QuizForm.fxml"));
-        stage.setScene(new Scene(root, 800, 600));
-        stage.setResizable(false);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
@@ -37,8 +35,7 @@ public class MenuController{
     private void switchToQuizGame(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/QuizGame.fxml"));
-        stage.setScene(new Scene(root, 800, 600));
-        stage.setResizable(false);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
