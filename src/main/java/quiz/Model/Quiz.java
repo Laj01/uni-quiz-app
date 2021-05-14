8ecb719f69314adbee4f0cd6e089e14001d94bea
Nagy Lajos <nagylali001@gmail.com>
2021-05-14 20:51:24 +0200

    Quiz wrapper class created

---
+++ src/main/java/quiz/Model/Quiz.java
@@ -0,0 +1,15 @@
+package quiz.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Quiz {
+
+    private List<Question> questions;
+}
